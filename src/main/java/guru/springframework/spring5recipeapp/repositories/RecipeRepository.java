0e652d1c720dad8ff9ac3b7ced500013806c7c07
udayanga91 <u.senanayake91@gmail.com>
2020-05-24 15:34:13 +0900

    Added Repositories

---
+++ src/main/java/guru/springframework/spring5recipeapp/repositories/RecipeRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.spring5recipeapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
+
+public interface RecipeRepository extends CrudRepository<Repository, Long> {
+}
